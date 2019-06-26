2aa4749d70354f9bd78c2cbb9dd84ef29e0391af
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-26 16:38:41 -0700

    created lib and tests folders, moved server to become app

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const { createServer } = require('http');
+const app = require('./lib/app');
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('this will have index.html data once I figure it out');
-});
 
 server.listen(7890, () => {
-  console.log('listening on 7890')
+  console.log('listening on 7890');
 });
 
