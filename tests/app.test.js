9fd2a80731b635c231ddb4f6600dec75fae9e48e
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-27 09:18:31 -0700

    fixed pathing errors and refactor code for first test with the help of Susan, wrote and passed second test to read file in public directory

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { readFile } = require('../lib/readFile');
 
 describe('http routes', () => {
   it('has a index.html route', () => {
     return request(app)
-      .get('/Users/lacrivella/Projects/careertrack/week-one/http-file-server/public/index.html')
+      .get('/index.html')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('Nolan'));
       });
+  });
+
+  it('reads file in public directory', done => {
+    readFile('./public/index.html', (err, content) => {
+      expect(err).toBeFalsy();
+      expect(content).toEqual(expect.any(String));
+      done();
+    });
   });
 });
