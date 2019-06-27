d11c36069285eb4478417f5cb6450925df7efdd3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-26 17:18:07 -0700

    moved readFile into it's own folder

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ const app = require('./lib/app');
 app.listen(7890, () => {
   console.log('listening on 7890');
 });
-
-const fs = require('fs');
-fs.readFile(__dirname + '/public/index.html', { encoding: 'utf8' }, (err, content) => {
-  if(err) return console.error(err);
-  console.log(content);
-});
